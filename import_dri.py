3b77f2324921e40aee165f64177b7491e5ebb72d
snakada <khf04440@nifty.com>
2019-09-21 22:44:32 +0900

    Add: NFA data added

---
+++ import_dri.py
@@ -0,0 +1,18 @@
+import sqlite3
+import csv
+
+con = sqlite3.connect('db.sqlite3')
+cursor = con.cursor()
+
+with open('DRI.csv', 'r') as f:
+    b = csv.reader(f)
+    print(b)
+    header = next(b)
+    sql = 'insert into myApp_DRI (age_id,male_protain,male_vitA,male_fe,female_protain,female_vitA,female_fe) VALUES(?,?,?,?,?,?,?)'
+    for t in b:
+        # tableに各行のデータを挿入する。
+        data = t
+        cursor.execute(sql, data)
+
+con.commit()
+con.close()
--- myApp/admin.py
+++ myApp/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from .models import feed
+from .models import FCT, DRI, DRI_women, Family, FamilyMember, Crop
 
 # Register your models here.
 
 admin.site.register(feed)
+admin.site.register(FCT)
+admin.site.register(DRI)
+admin.site.register(DRI_women)
+admin.site.register(Family)
+admin.site.register(FamilyMember)
+admin.site.register(Crop)
